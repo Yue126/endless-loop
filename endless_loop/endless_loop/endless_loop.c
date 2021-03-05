994b9de72d9f0e39299e9b51aa97ba8a6141df87
岳126 <320486541@qq.com>
2021-03-05 10:51:34 +0800

    提交代码

---
+++ endless_loop/endless_loop/endless_loop.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main(){
+	int i = 0;
+	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+	for(i = 0; i <= 12; i++){
+		printf("¹þ¹þ\n");
+		arr[i] = 0;
+	}
+	return 0;
+}
